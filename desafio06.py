5826ad280c6c32b1aeb8f1cf45fe41f9d5aa32ff
Marcos Rocha <61567855+socram63@users.noreply.github.com>
2026-03-20 20:52:29 +0000

    Mudando para 1 dígito após a vírgula

--- desafio06.py
+++ desafio06.py
@@ -1,2 +1,3 @@
 n=int(input('Digite um numero: '))
-print(f'o numero è {n}, seu dobro è {n*2}, seu triplo è {n*3} a a sua raiz quadrada é {n**0.5:.2f}')
+print(f'O numero è {n}, seu dobro è {n*2}\nSeu triplo è {n*3}\nA sua raiz quadrada é {pow(n,(1/2)):.2f}')
+# a a sua raiz quadrada é {n**0.5:.2f}
--- desafio07.py
+++ desafio07.py
@@ -1,3 +1,3 @@
 n1=float(input('Digite a primeira nota: '))
 n2=float(input('Digite a segunda nota '))
-print(f'A primiera nota è {n1}, a segunda nota è {n2} e a sua media ficou {(n1+n2)/2}')
+print(f'A primiera nota è {n1:.1f}, a segunda nota è {n2:.1f} e a sua media ficou {(n1+n2)/2:.1f}')
