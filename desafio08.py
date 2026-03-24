b7b3299fee141c2928874409056af98dc2aece68
Marcos Rocha <61567855+socram63@users.noreply.github.com>
2026-03-24 12:29:43 +0000

    deixando mais organizado e com mais opcoes

--- desafio08.py
+++ desafio08.py
@@ -1,2 +1,10 @@
 metros=float(input('Metros: '))
-print(f'{metros} metros sao {metros*100} centimetros e {metros*1000} milimetros')
+#print(f'{metros} metros sao {metros*100} centimetros e {metros*1000} milimetros')
+km= metros/1000
+hm= metros/100
+dam= metros/10
+dm= metros*10
+cm= metros*100
+mm= metros*1000
+
+print(f'{metros} Metros sao: \n{km} Km\n{hm} hm\n{dam} dam\n{dm:.0f} dm\n{cm:.0f} cm\n{mm:.0f} mm')
